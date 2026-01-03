4827c4a65e2bb199cbfe2e767ae5fbe6c43b357e
meherab31 <19303049@iubat.edu>
2026-01-04 00:35:58 +0600

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import { Button, ButtonGroup } from "@chakra-ui/react";
 
 const App = () => {
-  return;
-  <>
-    <Button colorScheme="blue">Button</Button>;
-  </>;
+  return <Button colorScheme="blue">Button</Button>;
 };
 
 export default App;
