602d0ea23250adc95f23bf25a262af5a9fe644b1
meherab31 <19303049@iubat.edu>
2026-01-04 00:50:32 +0600

    Welcome Screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Box, Heading, Text, VStack, Spinner } from "@chakra-ui/react";
+import { useEffect } from "react";
 
 const App = () => {
-  return <Button colorScheme="blue">Button</Button>;
+  useEffect(() => {
+    document.title = "Gamera - Find Your Desired Games";
+  }, []);
+  return (
+    <Box
+      minH="100vh"
+      bg="gray.50"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <VStack spacing={6} textAlign="center">
+        <Heading
+          size="2xl"
+          bgGradient="linear(to-r, blue.400, purple.500)"
+          bgClip="text"
+          fontWeight="bold"
+        >
+          Welcome to Gamera
+        </Heading>
+        <Text fontSize="lg" color="gray.600">
+          Website on progress
+        </Text>
+        <Spinner size="xl" color="blue.500" />
+      </VStack>
+    </Box>
+  );
 };
 
 export default App;
