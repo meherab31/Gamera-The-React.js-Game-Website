7d79bf2b8ffc32467187e5d4b1ec0dd56acd4c3a
meherab31 <19303049@iubat.edu>
2026-01-10 02:03:06 +0600

    nav design

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,23 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image, Text, Box, useColorModeValue } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
+  const bgColor = useColorModeValue("white", "gray.800");
+  const textColor = useColorModeValue("black", "white");
+
   return (
-    <HStack justifyContent={"space-between"} paddingX={5} paddingY={2}>
-      <Image src={logo} boxSize={"40px"}></Image>
-      <ColorModeSwitch></ColorModeSwitch>
-    </HStack>
+    <Box bg={bgColor} color={textColor} px={5} py={3} shadow="md">
+      <HStack justifyContent="space-between" align="center">
+        <HStack spacing={4} align="center">
+          <Image src={logo} boxSize="50px" borderRadius="full" />
+          <Text fontSize="2xl" fontWeight="bold" fontFamily="heading">
+            Gamera - Your Game Explorer
+          </Text>
+        </HStack>
+        <ColorModeSwitch />
+      </HStack>
+    </Box>
   );
 };
 
